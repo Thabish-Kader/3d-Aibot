6df43b35a07eb4c7a786836b20b8fbad9c1672eb
Thabish <kadertabish2@gmail.com>
2023-03-21 14:09:38 +0400

    speechsynthesis-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- components/TextToSpeech.tsx
+++ components/TextToSpeech.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export const TextToSpeech = () => {
-	return <div>TextToSpeech</div>;
+	const [text, setText] = useState("");
+	const synth = typeof window !== "undefined" ? window.speechSynthesis : null;
+	const voices = synth?.getVoices();
+
+	const seletedVoice = voices?.find((voice) => voice.name === "Albert");
+
+	const speak = () => {
+		const utterance = new SpeechSynthesisUtterance(text);
+		utterance.rate = -1;
+		utterance.voice = seletedVoice!;
+		synth?.speak(utterance);
+	};
+
+	return (
+		<div className="space-x-2 pt-2">
+			<input
+				type="text"
+				value={text}
+				className="bg-transparent border outline-none  rounded-lg placeholder:text-yellow-500 p-2 text-orange-500"
+				onChange={(e) => setText(e.target.value)}
+				placeholder="Enter text"
+			/>
+			<button
+				className="text-yellow-500 p-2 border rounded-lg"
+				onClick={speak}
+			>
+				Speak
+			</button>
+		</div>
+	);
 };
